5cf7a0b883065641fe1339906f5bbfc2416f7990
MarauderPixie <tobias.anton.85@gmail.com>
2020-04-11 13:56:40 +0200

    add package structure and hello world

---
+++ R/hello.R
@@ -0,0 +1,9 @@
+#' A function to greet the world.
+#'
+#' This function prints a friendly and enthusiastic greeting to the console!
+#' 
+#' @export
+
+hello <- function() {
+  print("Hello, world!")
+}
